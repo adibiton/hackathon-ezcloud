dd5bd54cf13735d3fffd82c1f2c77b8fe43ba488
Aviram Leder <avleder@microsoft.com>
2019-07-23 18:23:18 +0300

    does not work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as git from "./git";
 import * as build from "./build";
+import * as release from "./release";
 
 build.run()
+//release.run()
     .then(text => {
         console.log(text);
     })
