366cf556e8289c0abd058024a7880066fe560268
Adi Biton <adbiton@microsoft.com>
2019-07-24 12:54:51 +0300

    new commit

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,68 @@
 #!/usr/bin/env node
 
-require('dotenv').config()
 const program = require('commander');
-const deploy = require('../lib/deploy');
+const ora = require('ora');
+const chalk = require('chalk');
+const msRestAzure = require("ms-rest-azure");
+
+const { kubernetesAppsNamespace, kubernetesNamespace, clientId, applicationSecret, domain, appName } = require('../ezcloud.json');
+
+const Containerregistry = require('../lib/resources/acr');
+const ResourceGroup = require('../lib/resources/resource-group');
+const Aks = require('../lib/resources/aks');
+const KubernetesClient = require('../lib/kubectl-client');
+const RolesClient = require('../lib/roles-client');
+const ActiveDirectoryClient = require('../lib/active-directory-client');
+
+async function deploy() {
+    try {
+        const spinner = ora({ color: 'green', spinner: 'dots10' }).start();
+        const creds = await msRestAzure.loginWithServicePrincipalSecret(clientId, applicationSecret, domain);
+        //Create resource group
+        const resourceGroup = new ResourceGroup(creds);
+        await resourceGroup.create();
+
+        //Create conatainer registery
+        const containerRegistry = new Containerregistry(creds)
+        await containerRegistry.create();
+
+        //Create azure k8s service
+        const aks = new Aks(creds);
+        await aks.create();
+
+        //const adminCred = await aks.getAdminCredentials();
+        //Configure k8s
+        const kubernetesClient = new KubernetesClient();
+        await kubernetesClient.createServiceAccount(kubernetesNamespace);
+        await kubernetesClient.createClusterRoleBinding(kubernetesNamespace);
+        await kubernetesClient.createNamespace(kubernetesAppsNamespace);
+        await kubernetesClient.createClusterRoleBindingWithView(kubernetesAppsNamespace);
+
+        //Get AKS clientID
+        const aksClientId = await aks.getClientID();
+
+        //Get ACR ID
+        const acrId = await containerRegistry.getId();
+
+        //Assign relevant role to aks
+        const rolesClient = new RolesClient();
+        await rolesClient.createAssignment(aksClientId, acrId);
+
+        //Create active directory service principal
+        const activeDirectoryClient = new ActiveDirectoryClient();
+        const password = await activeDirectoryClient.createServicePrincipal(acrId);
+        // console.log(`Active directory password: ${password}`);
+        spinner.stop();
+        console.log(`Finished deploying ${appName} to Azure succesffully with ${chalk.blue('Ezcloud')}!`);
+
+    } catch (err) {
+        console.log(JSON.stringify(err));
+    }
+}
 
 program
     .command('deploy')
     .description('Deploy your app to Azure')
-    .action(function () {
-        deploy();
-    })
+    .action(deploy);
+
 program.parse(process.argv);
--- bin/test.js
+++ bin/test.js
@@ -1,24 +1,57 @@
 'use strict';
 
-const Containerregistry = require('../lib/resources/acr')
-const ResourceGroup = require('../lib/resources/resource-group')
+const { kubernetesAppsNamespace, kubernetesNamespace, clientId, applicationSecert, domain } = require('../ezcloud.json');
 
-const clientId = process.env.CLIENT_ID;
-const applicationSecert = process.env.APPLICATION_SECRET;
-const domain = process.env.DOMAIN;
+const Containerregistry = require('../lib/resources/acr');
+const ResourceGroup = require('../lib/resources/resource-group');
+const Aks = require('../lib/resources/aks');
+const KubernetesClient = require('../lib/kubectl-client');
+const RolesClient = require('../lib/roles-client');
+const ActiveDirectoryClient = require('../lib/active-directory-client');
 
 const msRestAzure = require("ms-rest-azure");
-
 async function run() {
-    const creds = await msRestAzure.loginWithServicePrincipalSecret(clientId, applicationSecert, domain);
-    //Create resource group
-    const resourceGroup = new ResourceGroup(creds)
-    const result = await resourceGroup.create()
-    console.log(`Created resource group successfully with the following result: ${JSON.stringify(result)}`)
-    //Create conatainer registery
-    const containerRegistry = new Containerregistry(creds)
-    const containerRegistryResult = await containerRegistry.create();
-    console.log(`Created container registry successfully with the following result: ${JSON.stringify(containerRegistryResult)}`);
+    try {
+        const creds = await msRestAzure.loginWithServicePrincipalSecret(clientId, applicationSecert, domain);
+        //Create resource group
+        const resourceGroup = new ResourceGroup(creds)
+        await resourceGroup.create()
+
+        //Create conatainer registery
+        const containerRegistry = new Containerregistry(creds)
+        await containerRegistry.create();
+
+        //Create azure k8s service
+        const aks = new Aks(creds);
+        await aks.create();
+
+        //const adminCred = await aks.getAdminCredentials();
+        //Configure k8s
+        const kubernetesClient = new KubernetesClient();
+        await kubernetesClient.createServiceAccount(kubernetesNamespace);
+        await kubernetesClient.createClusterRoleBinding(kubernetesNamespace);
+        await kubernetesClient.createNamespace(kubernetesAppsNamespace);
+        await kubernetesClient.createClusterRoleBindingWithView(kubernetesAppsNamespace);
+
+        //Get AKS clientID
+        const aksClientId = await aks.getClientID();
+
+        //Get ACR ID
+        const acrId = await containerRegistry.getId();
+
+        //Assign relevant role to aks
+        const rolesClient = new RolesClient();
+        await rolesClient.createAssignment(aksClientId, acrId);
+
+        //Create active directory service principal
+        const activeDirectoryClient = new ActiveDirectoryClient();
+        const password = await activeDirectoryClient.createServicePrincipal(acrId);
+        // console.log(password);
+
+    } catch (err) {
+        console.log(JSON.stringify(err));
+    }
 }
 
 run();
+
